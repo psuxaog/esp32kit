8213c588a68a4b1f0151f6678279a34b96dd5b87
psuxaog <psuxaog@126.com>
2019-01-16 09:04:25 +0800

    Update

--- github/firstproject/main/app_main.c
+++ github/firstproject/main/app_main.c
@@ -25,6 +25,7 @@
 #include "oled.h"
 #include "kitui.h"
 #include "smartcfg.h"
+#include "cJSON.h"
 /*
  * Global Val Def
  */
@@ -35,7 +36,24 @@ static EventGroupHandle_t EventGroupWiFi;
 static const int BIT_CONNECT_OK = BIT0;
 static const int BIT_IPV4_GOT_IP = BIT1;
 static const int BIT_IPV6_GOT_IP = BIT2;
+typedef struct
+{
+	struct
+	{
+		char *ip;
+		uint8_t mac[6];
+		char *ssid;
+	} Net;
+	struct
+	{
+		char *pn;
+		uint16_t id;
+		uint8_t type;
+	} Device;
+} DeviceInfo_t;
+DeviceInfo_t DeviceInfo;
 
+static void PushDevInfo(DeviceInfo_t *device);
 /*
  *
  */
@@ -51,6 +69,10 @@ static esp_err_t WiFiEventHandle(void *ctx, system_event_t *event)
 		break;
 	case SYSTEM_EVENT_STA_GOT_IP:
 		xEventGroupSetBits(EventGroupWiFi, BIT_CONNECT_OK);
+		DeviceInfo.Net.ip = ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip);
+		ESP_ERROR_CHECK(esp_read_mac(DeviceInfo.Net.mac,ESP_MAC_WIFI_STA))
+		;
+		PushDevInfo(&DeviceInfo);
 		ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
 		break;
 	case SYSTEM_EVENT_STA_DISCONNECTED:
@@ -63,6 +85,7 @@ static esp_err_t WiFiEventHandle(void *ctx, system_event_t *event)
 		ESP_LOGI(TAG, "SYSTEM_EVENT_AP_STA_GOT_IP6");
 		char *ip6 = ip6addr_ntoa(&event->event_info.got_ip6.ip6_info.ip);
 		ESP_LOGI(TAG, "IPv6:%s", ip6);
+
 		break;
 	default:
 		break;
@@ -79,12 +102,14 @@ static void AppUi_Task(void *parm)
 			switch (eKey)
 			{
 			case eKeyWheelDec:
+				ESP_LOGI(TAG, "eKeyWheelDec");
+				OLED_Print("Dec");
 				break;
 			case eKeyWheelInc:
+				ESP_LOGI(TAG, "eKeyWheelInc+++");
+				OLED_Print("Inc+++");
 				break;
 			case eKeyWheelLong:
-				break;
-			case eKeyWheelPress:
 				break;
 			case eKeyWheelRelease:
 				break;
@@ -123,6 +148,7 @@ static void wifi_init(void)
 	wifi_cfg.sta.scan_method = WIFI_FAST_SCAN;
 	ESP_LOGI(TAG, "WIFI SSID:%s", wifi_cfg.sta.ssid);
 	ESP_LOGI(TAG, "WIFI PASSWORD:%s", wifi_cfg.sta.password);
+	DeviceInfo.Net.ssid = (char *)&wifi_cfg.sta.ssid[0];
 	OLED_Print("WIFI SSID:%s", wifi_cfg.sta.ssid);
 	OLED_Print("WIFI PASSWORD:%s", wifi_cfg.sta.password);
 	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
@@ -133,6 +159,40 @@ static void wifi_init(void)
 	xEventGroupWaitBits(EventGroupWiFi, BIT_CONNECT_OK, false, true,
 	portMAX_DELAY);
 	ESP_LOGI(TAG, "Connect OK");
+}
+static void PushDevInfo(DeviceInfo_t *device)
+{
+	cJSON *root = NULL;
+	cJSON *objDev = NULL;
+	cJSON *objNet = NULL;
+	char *macstr = pvPortMalloc(18);
+	root = cJSON_CreateObject();
+	cJSON_AddItemToObject(root, "netcfg", objNet = cJSON_CreateObject());
+	sprintf(macstr, "%2X:%2X:%2X:%2X:%2X:%2X", device->Net.mac[0],
+			device->Net.mac[1], device->Net.mac[2], device->Net.mac[3],
+			device->Net.mac[4], device->Net.mac[5]);
+	cJSON_AddStringToObject(objNet, "mac", macstr);
+	vPortFree(macstr);
+	if (device->Net.ip != NULL)
+	{
+		cJSON_AddStringToObject(objNet, "ip", device->Net.ip);
+	}
+	if (device->Net.ssid != NULL)
+	{
+		cJSON_AddStringToObject(objNet, "ssid", device->Net.ssid);
+	}
+
+	cJSON_AddItemToObject(root, "device", objDev = cJSON_CreateObject());
+
+	if (device->Device.pn != NULL)
+	{
+		cJSON_AddStringToObject(objDev, "pn", device->Device.pn);
+	}
+	cJSON_AddNumberToObject(objDev, "id", device->Device.id);
+	cJSON_AddNumberToObject(objDev, "type", device->Device.type);
+	/* Print to text */
+	printf("cJson Begin:\r\n%s\r\nEnd", cJSON_Print(root));
+	cJSON_Delete(root);
 }
 void app_main()
 {
@@ -146,8 +206,7 @@ void app_main()
 	OLED_Print("Target:%s", CONFIG_BOARD_TARGET);
 	xKeyQueue = xQueueCreate(2, sizeof(eKey_t));
 	vUI_Init(&xKeyQueue);
-	xTaskCreate(vUI_Task, "UI_Task", 1280, NULL, 5, NULL);
-	xTaskCreate(AppUi_Task, "AppUI_Task", 4096, NULL, 5, NULL);
+	xTaskCreate(vUI_Task, "UI_Task", 4096, NULL, 5, NULL);
+	xTaskCreate(AppUi_Task, "AppUI_Task", 4096, NULL, 4, NULL);
 	wifi_init();
-
 }
